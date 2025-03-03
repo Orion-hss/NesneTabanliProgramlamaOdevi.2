dbb564b90cc3d6781bf6b068e084b614b3bab11a
Huriye Şenal <huriyeders@gmail.com>
2025-03-03 11:24:50 +0300

    Add files via upload

---
+++ ödevv1.cpp
@@ -0,0 +1,32 @@
+﻿
+#include <iostream>
+using namespace std;
+
+/*
+a + b = toplama
+a - b = cikarma
+a * b = carpma
+a / b = bolme
+a % z = mod
+*/
+int main(){
+    int a, z;
+    float b;
+    cout << "a sayisini girin";
+    cin >> a;
+    cout << "b sayisini girin";
+    cin >> b;
+    cout << "z sayisini girin";
+    cin >> z;
+    cout << "toplama:" << ( a + b) << "| sizeof(a + b)=" <<sizeof(a + b) << endl;
+    cout << "cikarma:" << (a - b) << "| sizeof(a - b)=" << sizeof(a - b) << endl;
+    cout << "carpma:" << (a * b) << "| sizeof(a * b)=" << sizeof(a * b) << endl;
+    cout << "bolme:" << (a / b) << "| sizeof(a / b)=" << sizeof(a / b) << endl;
+    cout << "mod:" << (a % z) << "| sizeof(a % z)=" << sizeof(a % z) << endl;
+
+
+
+    return 0;
+    
+}
+
---
+++ ödevv2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string ad;
+    string soyAd;
+    string meslek;
+
+    // Kullanıcı bilgileri
+    cout << "Adinizi giriniz: ";
+    getline(cin, ad);
+
+    cout << "Soyadinizi giriniz: ";
+    getline(cin, soyAd);
+
+    cout << "Mesleginizi giriniz: ";
+    getline(cin, meslek);
+
+    cout << "....................." << endl;
+    cout << "Kisi Bilgileri" << endl;
+    cout << "....................." << endl;
+
+    cout << ".. Ad: "     << ad     << "  .." << endl;
+    cout << ".. Soyad: "  << soyAd  << "  .." << endl;
+    cout << ".. Meslek: " << meslek << "  .." << endl << endl;
+
+    string tamAd = ad + soyAd;
+
+    cout << "..Uzunluk: " << tamAd.size() << "  .." << endl << endl;
+
+    // tanıtım metni oluşturdum
+    string tanitim = "Merhaba, ben " + ad + soyAd + meslek + " olarak çalışıyorum.";
+
+    // Tanıtım metni ekrana yazdırdım
+    cout << "Tanitim: " << tanitim << endl;
+
+    return 0;
+}
